e9050c5babc2d5770d8786c716cbd16ccdfcaf3b
henri-priest <abc@example.com>
2018-10-18 15:43:09 -0500

    saving work

--- terraform/site.tf
+++ terraform/site.tf
@@ -33,9 +33,54 @@ resource "aws_s3_bucket" "bucket2" {
 
 }
 
-#resource "aws_s3_bucket_object" "index" {
-#  key        = "index.html"
-#  bucket     = "${aws_s3_bucket.bucket2.id}"
-#  source     = "./index.html"
-#  content_encoding = "text/html"
-#}
+resource "aws_cloudfront_origin_access_identity" "test" {
+  comment = "Some comment"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = "${aws_s3_bucket.bucket2.bucket_regional_domain_name}"
+    origin_id   = "${aws_s3_bucket.bucket2.bucket_regional_domain_name}"
+
+    s3_origin_config {
+      origin_access_identity = "${aws_cloudfront_origin_access_identity.test.id}"
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+  default_root_object = "index.html"
+
+  aliases = ["dottie-test.com", "www.dottie-test.com"]
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "${aws_s3_bucket.bucket2.bucket_regional_domain_name}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+}
